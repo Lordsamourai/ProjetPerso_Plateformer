f4121791db0403407ac12770e14ea048b84c2f56
Lordsamourai <157367778+Lordsamourai@users.noreply.github.com>
2025-01-29 09:58:30 +0100

    Update

---
+++ Assets/Script/WorldLimit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldLimit : MonoBehaviour
+{
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DeplacementPersonnage player = other.GetComponent<DeplacementPersonnage>();
+            if (player != null)
+            {
+                StartCoroutine(ApplySpeedBoost(player));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Test sortie de terrain 
+        if ((positionRoute.position.y - transform.position.y) > player.GetComponent<Renderer>().bounds.size.y)
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene("ENDscene");
+
+    }*/
+}
